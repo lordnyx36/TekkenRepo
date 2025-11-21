bd00f4c437ecd0253740f7cc975050c4d8731f7f
Karan Khatiwada <karankhatiwada36@gmail.com>
2025-11-21 13:16:42 +0545

    Added new Class called MathOperations for WEEK-3 workshop

---
+++ MathOperations.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class MathOperations here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MathOperations
+{
+    public static void main(String[] args)
+    {
+        //Arthematic
+        int a = 10;
+        int b = 5;
+        System.out.println("Arthematic Operators\n\n");
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+    }
+}
